e712931a06d142911a8ce9afc89fb48e6cad9870
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-10-17 22:15:34 +0700

    fix: ganti individu jadi array of int, tested

--- src/main/GeneticAlgorithm.java
+++ src/main/GeneticAlgorithm.java
@@ -1,40 +1,89 @@
 package main;
 
+import java.util.Arrays;
 import java.util.Random;
 
 class GeneticAlgorithm {
-    public String[] generateInitialPopulation() {
-        String[] initialPopulation = new String[4];
+    public int[][] generateInitialPopulation(int rounds) {
+        int[][] initialPopulation = new int[4][rounds];
+        Random random = new Random();
 
         for (int i = 0; i < 4; i++) {
-            StringBuilder individual = new StringBuilder(64);
-            Random random = new Random();
-            for (int j = 0; j < 64; j++) {
-                individual.append(random.nextInt(8) + 1);;
+            for (int j = 0; j < rounds; j++) {
+                int position = random.nextInt(64) + 1;
+                initialPopulation[i][j] = position;
             }
-            initialPopulation[i] = individual.toString();
         }
 
         return initialPopulation;
     }
 
-    public int getFitnessValue(String individu) {
-        // TODO
-        return 1;
+    public int countMarks(char[][] board, char mark) {
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == mark) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
-    public String[] selection() {
-        String[] population = this.generateInitialPopulation();
+    public int getFitnessValue(int[] individual) {
+        char[][] board = new char[8][8];
+        int currentPlayer = 1;
+
+        for (int i = 0; i < individual.length; i++) {
+            int position = individual[i];
+            int row = (position - 1) / 8;
+            int col = (position - 1) % 8;
+
+            if (currentPlayer == 1) {
+                board[row][col] = 'X';
+            } else {
+                board[row][col] = 'O';
+            }
+
+            for (int dr = -1; dr <= 1; dr++) {
+                for (int dc = -1; dc <= 1; dc++) {
+                    int newRow = row + dr;
+                    int newCol = col + dc;
+                    if (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+                        char neighborMark = board[newRow][newCol];
+                        if (neighborMark != ' ' && neighborMark != board[row][col]) {
+                            board[newRow][newCol] = board[row][col];
+                        }
+                    }
+                }
+            }
+
+            currentPlayer = 3 - currentPlayer;
+        }
+
+        int botCount = countMarks(board, 'X');
+        int playerCount = countMarks(board, 'O');
+        int fitnessValue = botCount - playerCount;
+
+        return fitnessValue;
+    }
+
+
+
+    public int[][] selection(int rounds) {
+        int[][] population = this.generateInitialPopulation(rounds);
         Random random = new Random();
-        String[] parents = new String[4];
+        int[][] parents = new int[4][];
+
+        int total = this.getFitnessValue(population[0]) + this.getFitnessValue(population[1]) + this.getFitnessValue(population[2]) + this.getFitnessValue(population[3]);
+
+        int rangeA = this.getFitnessValue(population[0]) * 100 / total;
+        int rangeB = rangeA + this.getFitnessValue(population[1]) * 100 / total;
+        int rangeC = rangeB + this.getFitnessValue(population[2]) * 100 / total;
+        // int rangeD = rangeC + this.getFitnessValue(population[3]);
 
         for (int i = 0; i < 4; i++) {
             int randomValue = random.nextInt(101);
-
-            int rangeA = this.getFitnessValue(population[0]);
-            int rangeB = rangeA + this.getFitnessValue(population[1]);
-            int rangeC = rangeB + this.getFitnessValue(population[2]);
-            // int rangeD = rangeC + this.getFitnessValue(population[3]);
 
             if (randomValue <= rangeA) {
                 parents[i] = population[0];
@@ -50,23 +99,61 @@ class GeneticAlgorithm {
         return parents;
     }
 
-    public String[] crossOver(String parent1, String parent2) {
-        int crossoverPoint = parent1.length() / 2;
+    public int[][] crossover(int[] parent1, int[] parent2) {
+        int crossoverPoint = parent1.length / 2;
+        int[] child1 = new int[parent1.length];
+        int[] child2 = new int[parent1.length];
 
-        String child1 = parent1.substring(0, crossoverPoint) + parent2.substring(crossoverPoint);
-        String child2 = parent2.substring(0, crossoverPoint) + parent1.substring(crossoverPoint);
+        for (int i = 0; i < crossoverPoint; i++) {
+            child1[i] = parent1[i];
+            child2[i] = parent2[i];
+        }
 
-        String[] children = {child1, child2};
+        for (int i = crossoverPoint; i < parent1.length; i++) {
+            child1[i] = parent2[i];
+            child2[i] = parent1[i];
+        }
+        int[][] children = {child1, child2};
+
         return children;
     }
 
-    public String mutate(String individual) {
+    public int[] mutate(int[] individual) {
         Random random = new Random();
-        int mutationValue = random.nextInt(10);
+        int mutationIndex = random.nextInt(individual.length);
+        int mutationValue = random.nextInt(64) + 1;
+        int[] mutatedIndividual = Arrays.copyOf(individual, individual.length);
+        mutatedIndividual[mutationIndex] = mutationValue;
 
-        StringBuilder mutatedIndividual = new StringBuilder(individual);
-        mutatedIndividual.setCharAt(2, (char) ('0' + mutationValue));
+        return mutatedIndividual;
+    }
 
-        return mutatedIndividual.toString();
+    public static void main(String[] args) {
+        // GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
+        // int rounds = 28; //
+
+        // int[][] initialPopulation = geneticAlgorithm.generateInitialPopulation(rounds);
+
+        // // Menampilkan populasi awal
+        // System.out.println("Initial Population:");
+        // for (int i = 0; i < initialPopulation.length; i++) {
+        //     System.out.println("Individual " + (i + 1) + ": " + Arrays.toString(initialPopulation[i]));
+        // }
+
+        // int rounds = 28; // Ganti dengan jumlah ronde yang sesuai
+
+        // int[][] initialPopulation = geneticAlgorithm.generateInitialPopulation(rounds);
+
+        // // Menampilkan populasi awal
+        // System.out.println("Initial Population:");
+        // for (int i = 0; i < 4; i++) {
+        //     System.out.println("Individual " + (i + 1) + ": " + Arrays.toString(initialPopulation[i]));
+        // }
+
+        // // Menghitung nilai fitness untuk setiap individu dalam populasi awal
+        // for (int i = 0; i < 4; i++) {
+        //     int fitnessValue = geneticAlgorithm.getFitnessValue(initialPopulation[i]);
+        //     System.out.println("Fitness Value for Individual " + (i + 1) + ": " + fitnessValue);
+        // }
     }
 }
