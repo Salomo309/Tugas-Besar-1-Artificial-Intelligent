a44f6d4021613e4ccf8130afa075023c4452d3d6
Mifkiyan <m.favian199@gmail.com>
2023-10-18 22:52:47 +0700

    fix: handle bot vs bot on minimax

--- src/main/Minimax.java
+++ src/main/Minimax.java
@@ -6,6 +6,8 @@ public class Minimax implements BotAlgorithm {
 
     @Override
     public int[] getBestMove(char[][] board, char mark) {
+        char markBot = mark;
+        char markEnemy = mark == 'X' ? 'O' : 'X';
 
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
@@ -23,7 +25,7 @@ public class Minimax implements BotAlgorithm {
         BoardIterator it = new BoardIterator(board, mark);
         char[][] state;
         while ((state = it.next()) != null) {
-            int score = minimax(state, 3, Integer.MIN_VALUE, Integer.MAX_VALUE, mark == 'X');
+            int score = minimax(state, 3, Integer.MIN_VALUE, Integer.MAX_VALUE, false, markBot, markEnemy);
             // System.out.printf("Score for move (%d, %d): %d\n", it.getMove()[0], it.getMove()[1], score);
             if (score > bestScore) {
                 bestScore = score;
@@ -33,17 +35,17 @@ public class Minimax implements BotAlgorithm {
         return bestMove;
     }
 
-    public int minimax(char[][] board, int depth, int alpha, int beta, boolean isMaximizing) {
+    public int minimax(char[][] board, int depth, int alpha, int beta, boolean isMaximizing, char markBot, char markEnemy) {
         if (depth == 0) {
-            return evaluate(board);
+            return evaluate(board, markBot, markEnemy);
         }
 
         if (isMaximizing) {
             int maxScore = Integer.MIN_VALUE;
-            BoardIterator it = new BoardIterator(board, 'O');
+            BoardIterator it = new BoardIterator(board, markBot);
             char[][] state;
             while((state = it.next()) != null) {
-                int score = minimax(state, depth - 1, alpha, beta, false);
+                int score = minimax(state, depth - 1, alpha, beta, false, markBot, markEnemy);
                 maxScore = Math.max(maxScore, score);
                 alpha = Math.max(alpha, score);
                 if (beta <= alpha) {
@@ -53,10 +55,10 @@ public class Minimax implements BotAlgorithm {
             return maxScore;
         } else {
             int minScore = Integer.MAX_VALUE;
-            BoardIterator it = new BoardIterator(board, 'X');
+            BoardIterator it = new BoardIterator(board, markEnemy);
             char[][] state;
             while((state = it.next()) != null) {
-                int score = minimax(state, depth - 1, alpha, beta, true);
+                int score = minimax(state, depth - 1, alpha, beta, true, markBot, markEnemy);
                 minScore = Math.min(minScore, score);
                 beta = Math.min(beta, score);
                 if (beta <= alpha) {
@@ -67,15 +69,15 @@ public class Minimax implements BotAlgorithm {
         }
     }
 
-    private int evaluate(char[][] board) {
+    private int evaluate(char[][] board, char markBot, char markEnemy) {
         int botScore = 0;
         int opponentScore = 0;
 
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
-                if (board[i][j] == 'O') {
+                if (board[i][j] == markBot) {
                     botScore++;
-                } else if (board[i][j] == 'X') {
+                } else if (board[i][j] == markEnemy) {
                     opponentScore++;
                 }
             }
